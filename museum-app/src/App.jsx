dfefbefb9f1dfee86c1703dc77179f691e12258c
unknown <enseignement.martinruppdahlem@gmail.com>
2024-12-03 19:00:09 +0100

    feat: ajout du loader

--- museum-app/src/App.jsx
+++ museum-app/src/App.jsx
@@ -5,27 +5,28 @@ import ImageContainer from './components/ImageContainer'
 
 function App() {
   const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const apiKey = import.meta.env.VITE_API_RIJKS_API_KEY
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchData = async () => {
       const response = await fetch(`https://www.rijksmuseum.nl/api/en/collection?key=${apiKey}`);
       const result = await response.json();
       setData(result);
+      setIsLoading(false);
     };
 
     fetchData();
-
-    console.log(JSON.stringify(data))
   }, []);
 
 
   return (
     <>
-    <div className="appContent">
-       <ImageContainer imagesData={data.artObjects}/>
-    </div>
+      <div className="appContent">
+        {isLoading ? "LOADING" : <ImageContainer imagesData={data.artObjects} />}
+      </div>
     </>
   )
 }
